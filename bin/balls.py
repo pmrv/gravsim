85d65e225b448e6e32ce9c636fac4abc57bebc99
Ponder Stibbons <ponder@creshal.de>
2013-01-16 15:39:23 +0100

    finally a working, generalized method to bounce stuff
    I AM AWESOME.

--- bin/balls.py
+++ bin/balls.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.locals import *
-from math import ceil, fabs
+from math import ceil
 
 from gravsim.things import Ball
 from gravsim.simulation import Simulation
@@ -9,14 +9,13 @@ HEIGHT = 400
 WIDTH  = 400
 RAD    =  10
 
-WHITE  = Color (200, 200, 200)
+WHITE  = Color (255, 255, 255)
 BLACK  = Color (000, 000, 000)
 
 CLOCK = pygame.time.Clock ()
 DISPLAY = pygame.display.set_mode ((HEIGHT, WIDTH))
 
-balls = ( Ball (RAD, (200, 200), (10, 5)), )
-#Ball (RAD, (100, 100), (0, 0)))
+balls = ( Ball (RAD, (100, 70), (10, 10)), Ball (RAD, (50, 100), (10, 0)))
 sim = Simulation (balls, (HEIGHT, WIDTH), .1)
 
 while True:
--- gravsim/simulation.py
+++ gravsim/simulation.py
@@ -1,3 +1,5 @@
+from math import fabs
+from time import sleep
 from gravsim.vec2d import vec2d
 
 class Simulation (object):
@@ -24,46 +26,34 @@ class Simulation (object):
 
         for thing in self.things: thing.accelerate ("g", self.g)
 
-        #things [0].accelerate ('a', vec2d (0, -11))
+        things [0].accelerate ('a', vec2d (0, -11))
 
         x1wall = (vec2d (0, 0), vec2d (fieldsize [0], 0))
         x2wall = (vec2d (0, fieldsize [1]), vec2d (fieldsize [0], 0))
         y1wall = (vec2d (0, 0), vec2d (0, fieldsize [1]))
         y2wall = (vec2d (fieldsize [0], 0), vec2d (0, fieldsize [1]))
+        diagon = (vec2d (0, 0), vec2d (fieldsize))
 
-        self.walls (x1wall, x2wall, y1wall, y2wall)
+        self.walls = (x1wall, x2wall, y1wall, y2wall, diagon)
 
     def step (self):
 
         for thing in self.things:
             self.time += self.stepsize
+            thing.move (self.stepsize)
 
             trad = thing.radius
-            nextpos = thing.position + thing.velocity * t
 
             for premise, border in self.walls:
 
-                radius_vector = 
-                present_angle = border.get_angle_between (
-
-
-            for axis, wall in enumerate ( (self.ywall, self.xwall) ):
-
-                other = (axis + 1) % 2
-                if (wall + (thing [axis], -trad)) [other] < thing [other]:
-                    thing.mirror_velocity (other)
-                    thing [other] = wall [other] - thing.radius - 1
-                    break
-
-                # another generalization needed to get rid of this
-                elif thing [other] - trad < 1:
-                    thing.mirror_velocity (other)
-                    thing [other] = 0 + thing.radius + 1
-                    break
-        else:
-            thing.move (self.stepsize)
-
-    def __repr__ (self):
-        return self.position.__repr__ () + self.velocity.__repr__ ()
-
-    __str__ = __repr__
+                radius_vector = trad * border.perpendicular_normal ()
+                
+                present_angle = border.get_angle_between (thing.position - premise + radius_vector)
+                future_angle = border.get_angle_between (
+                        thing.position + thing.velocity * self.stepsize / 2 - premise - radius_vector
+                )
+                sign_p = present_angle / fabs (present_angle) if present_angle != 0 else 1
+                sign_f = future_angle / fabs (future_angle) if future_angle != 0 else -1 * sign_p
+                if sign_p != sign_f or present_angle == 0:
+                    v_angle = border.get_angle_between (thing.velocity)
+                    thing.velocity.rotate (-1 * 2 * v_angle)
