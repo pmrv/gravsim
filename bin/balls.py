7790c828b40022126a751a48fa6380de55814ae4
Ponder Stibbons <ponder@creshal.de>
2013-02-02 17:24:48 +0100

    well, meh

--- bin/balls.py
+++ bin/balls.py
@@ -18,9 +18,8 @@ BLACK  = Color (000, 000, 000)
 CLOCK = pygame.time.Clock ()
 DISPLAY = pygame.display.set_mode ((WIDTH, HEIGHT), RESIZABLE)
 
-FACTOR = Decimal ("1")
 DISPLAY_BORDER = 50 
-MAX_DISPLAY_LENGTH = Decimal (min (WIDTH, HEIGHT)) / 2
+MAX_DISPLAY_LENGTH = Decimal (min (WIDTH, HEIGHT)) / 2 - DISPLAY_BORDER
 balls  = (Ball (RAD, 100, (-50, 0), (40, 0)), Ball (RAD, 1, (20, 19), (-40, 0)))#, Ball (RAD, 10, (50, 300), (0, -15)))
 earth = Ball (6371000, 10e24, (0, 0), (0, 0))
 moon  = Ball (1737100, 10e21, (0, 20000000), (20220000, 0))
@@ -29,12 +28,21 @@ solar = (earth, astro,)# moon)
 things = balls
 sim = Simulation (things, .01)
 
+factor = Decimal ("1")
+
 while True:
 
     DISPLAY.fill (WHITE)
-    pygame.draw.line (DISPLAY, BLACK, (0, HEIGHT / 2), (WIDTH, HEIGHT / 2))
-    pygame.draw.line (DISPLAY, BLACK, (WIDTH / 2, 0), (WIDTH / 2, HEIGHT))
+
+    grav_center = \
+        sum (t.mass * t.position for t in sim.things) / sum (t.mass for t in sim.things)
     max_position = 0
+    #display_center = (WIDTH / 2, HEIGHT / 2) - grav_center
+
+    pygame.draw.line (DISPLAY, BLACK, (0, grav_center [1]), 
+            (WIDTH, grav_center [1]))
+    pygame.draw.line (DISPLAY, BLACK, (grav_center [0], 0), 
+            (grav_center [0], HEIGHT))
 
     for event in pygame.event.get ():
         if event.type == QUIT:
@@ -46,15 +54,19 @@ while True:
             DISPLAY = pygame.display.set_mode ((WIDTH, HEIGHT), RESIZABLE)
             MAX_DISPLAY_LENGTH = Decimal (min (WIDTH, HEIGHT)) / 2
 
-    for b in sim.things:
-        if b.position.length > max_position:
-            max_position = b.position.length
+    for t in sim.things:
+        print (t.position)
+        if t.position.length > max_position:
+            max_position = t.position.length
 
+        display_pos = t.position - grav_center
         pygame.draw.circle (DISPLAY, BLACK, 
-                (ceil (b [0] * FACTOR) + WIDTH / 2, ceil (b [1]) * FACTOR + HEIGHT / 2), b.radius * FACTOR)
-    sim.step ()
+                (display_pos [0] * factor + WIDTH / 2,
+                 display_pos [1] * factor + HEIGHT / 2),
+                t.radius * factor)
 
-    #FACTOR = (MAX_DISPLAY_LENGTH - DISPLAY_BORDER)  / max_position
+    #factor = MAX_DISPLAY_LENGTH / max_position
+    sim.step ()
 
     pygame.display.update ()
     CLOCK.tick (60)
