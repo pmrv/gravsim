268aff1db9a8726d8f4e216f5cae81c4a2572599
Marvin Poul <ponder@creshal.de>
2013-08-20 16:48:02 +0200

    add __init__.py & nice up Sim.step a bit

--- gravsim/sim.py
+++ gravsim/sim.py
@@ -1,6 +1,7 @@
 from numpy import float64, array, sqrt
 from numpy.core.numeric import outer
 from numpy.core.multiarray import zeros
+from numpy.lib.shape_base import dstack
 from numpy.lib.npyio import loadtxt
 from time import time
 import csv
@@ -16,7 +17,8 @@ class Sim:
             reader = csv.reader (world)
             names = {}
             for k, line in enumerate (reader):
-                names [line [0]] = k
+                if not "#" in line [0]:
+                    names [line [0]] = k
 
         data = loadtxt (worldfile, delimiter = ',', usecols = range (1, 7), dtype = float64)
         self.names = names
@@ -29,6 +31,7 @@ class Sim:
         self.mm = outer (self.masses, self.masses)
         self.diagind = tuple (range (0, len (self.accelerations)))
 
+        self.count = len (self.names)
         self.time = 0
 
     def step (self, t):
@@ -37,14 +40,15 @@ class Sim:
 
         accdiag = self.accelerations [ (self.diagind, self.diagind) ]
 
-        dp = array ( [self.positions - self.positions [i] for i in range (len (self.positions))], dtype = float64 )
+        dp = array ( [self.positions - self.positions [i] for i in range (self.count], dtype = float64 )
         r3 = (dp**2).sum (axis = -1) ** (3/2) / self.mm
-        r3 = array ( (r3, r3) ).swapaxes (0, -1)
+        r3 = dstack ( (r3, r3) )
 
         self.accelerations [:,:,:] = self.G * dp / r3
-        self.accelerations [0] /= self.masses [0]
-        self.accelerations [1] /= self.masses [1]
-        self.accelerations [2] /= self.masses [2]
+        # there is probably a cooler way to do this..
+        for i in range (self.count):
+            self.accelerations [i] /= self.masses [i]
+
         self.accelerations [ (self.diagind, self.diagind) ] = accdiag
 
         a = self.accelerations.sum (axis = 1)
